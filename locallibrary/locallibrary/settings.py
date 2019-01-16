a6a23e558306641b4ed8d2d032783c5dfa880159
shtsai <shtsai@umich.com>
2019-01-16 17:42:23 +0000

    After Skeleton

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'restfy)-=gizi^)oy%2k#(x!y0(3&rl2$rvrvkq0nu)2nyn#fu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
