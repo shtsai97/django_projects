a3157bc562f4b595459b84b99967e00503e14f32
shtsai <shtsai@umich.com>
2019-04-10 04:54:18 +0000

    AD2

--- adlist/ads/views.py
+++ adlist/ads/views.py
@@ -1,4 +1,4 @@
-from ads.models import Ad
+from ads.models import Ad, Comment
 
 from django.views import View
 from django.views import generic
@@ -6,6 +6,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
+from ads.forms import CreateForm
+from ads.forms import CommentForm
 
 
 from ads.util import AdsListView, AdsDetailView, AdsCreateView, AdsUpdateView, AdsDeleteView
@@ -19,16 +21,92 @@ class AdDetailView(AdsDetailView):
     model = Ad
     template_name = "ad_detail.html"
 
+    def get(self, request, pk) :
+        ad = Ad.objects.get(id=pk)
+        comments = Comment.objects.filter(ad=ad).order_by('-updated_at')
+        comment_form = CommentForm()
+        context = { 'ad' : ad, 'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
+
 class AdCreateView(AdsCreateView):
-    model = Ad
-    fields = ['title', 'price', 'text']
     template_name = "ad_form.html"
+    success_url = reverse_lazy('ads')
+    def get(self, request, pk=None) :
+        form = CreateForm()
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk=None) :
+        form = CreateForm(request.POST, request.FILES or None)
+
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        # Add owner to the model before saving
+        ad = form.save(commit=False)
+        ad.owner = self.request.user
+        ad.save()
+        return redirect(self.success_url)
 
 class AdUpdateView(AdsUpdateView):
-    model = Ad
-    fields = ['title', 'price', 'text']
     template_name = "ad_form.html"
+    success_url = reverse_lazy('ads')
+    def get(self, request, pk) :
+        pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(instance=pic)
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk=None) :
+        pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(request.POST, request.FILES or None, instance=pic)
+
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        pic.save()
+        return redirect(self.success_url)
 
 class AdDeleteView(AdsDeleteView):
     model = Ad
     template_name = "ad_delete.html"
+
+def stream_file(request, pk) :
+    pic = get_object_or_404(Ad, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = pic.content_type
+    response['Content-Length'] = len(pic.picture)
+    response.write(pic.picture)
+    return response
+
+class AdFormView(LoginRequiredMixin, View):
+    template = 'ad_form.html'
+    success_url = reverse_lazy('ads')
+    def get(self, request, pk=None) :
+        if not pk :
+            form = CreateForm()
+        else:
+            pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+            form = CreateForm(instance=pic)
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk=None) :
+        if not pk:
+            form = CreateForm(request.POST, request.FILES or None)
+        else:
+            pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+            form = CreateForm(request.POST, request.FILES or None, instance=pic)
+
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        # Adjust the model owner before saving
+        pic = form.save(commit=False)
+        pic.owner = self.request.user
+        pic.save()
+        return redirect(self.success_url)
+
